97d1f173ef0a290f4a6203a5e82eca9d6185b813
DiegoKernel <gonzalezdiego.contact@gmail.com>
2022-09-22 18:58:13 -0300

    Primer ejercicio resuelto

--- 01.js
+++ 01.js
@@ -11,7 +11,11 @@ function soloNumeros(array) {
   // soloNumeros([1, 'Henry', 2]) debe retornar [1, 2]
 
   // Tu código aca:
-
+  return array.filter((element) =>{
+    if(Number(element)) {
+      return element;
+    }
+  })
 }
 
 // No modifiques nada debajo de esta linea //
